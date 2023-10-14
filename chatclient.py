0d363fded8f440bf37d9df01395d71769ea5b64f
Majed Biazid <m.biazid@autobia.sa>
2023-10-14 23:29:09 +0300

    Adding comments

--- chatclient.py
+++ chatclient.py
@@ -2,38 +2,42 @@ import socket
 import threading
 import time
 
-
+# Function to receive messages from the server
 def receive_messages(client_socket):
     while True:
         message = client_socket.recv(1024).decode('utf-8')
         print(message)
 
-
+# Function to send alive messages to the server at regular intervals
 def send_alive_messages(client_socket):
     while True:
         time.sleep(10)  # Send alive message every 10 seconds
         client_socket.send('Alive'.encode('utf-8'))
 
-
+# The main function
 def main():
+    # Create a client socket and connect to the server
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect(('localhost', 12345))
 
+    # Get the client's ID and send it to the server
     client_id = input("Enter your ID: ")
     client_socket.send(client_id.encode('utf-8'))
 
+    # Start a thread to receive messages from the server
     receive_thread = threading.Thread(
         target=receive_messages, args=(client_socket,))
     receive_thread.start()
 
+    # Start a thread to send alive messages to the server
     alive_thread = threading.Thread(
         target=send_alive_messages, args=(client_socket,))
     alive_thread.start()
 
     while True:
+        # Continuously read user input and send it to the server
         message = input()
         client_socket.send(message.encode('utf-8'))
-
 
 if __name__ == "__main__":
     main()
--- chatserver.py
+++ chatserver.py
@@ -2,13 +2,17 @@ import socket
 import threading
 import time
 
+# Dictionary to store connected clients and their last alive timestamps
 clients = {}
 clients_last_alive = {}
+
+# Lock to ensure safe access to the clients and clients_last_alive dictionaries
 clients_lock = threading.Lock()
 
 # Set the timeout interval for receiving "Alive" messages (in seconds)
 ALIVE_TIMEOUT = 10
 
+# Function to handle incoming messages from clients
 def handle_client(client_socket, client_id):
     global clients, clients_last_alive
     while True:
@@ -35,8 +39,8 @@ def handle_client(client_socket, client_id):
             remove_client(client_id, client_socket)
             break
 
+# Function to parse a message that has the format: (target_client_id) message-content
 def parse_message(message):
-    # Parse the general message format: (target_client_id) message-content
     if message.startswith('(') and ')' in message:
         start_idx = message.index('(') + 1
         end_idx = message.index(')')
@@ -45,12 +49,14 @@ def parse_message(message):
         return target_client_id, msg_content
     return None, None
 
+# Function to send the list of online clients to a specific client
 def send_client_list(client_socket):
     global clients
     with clients_lock:
         client_list = ', '.join(list(clients.keys()))
         client_socket.send(client_list.encode('utf-8'))
 
+# Function to send a message from one client to another
 def send_message(source_client_id, target_client_id, message_content, client_socket):
     global clients
     with clients_lock:
@@ -63,31 +69,23 @@ def send_message(source_client_id, target_client_id, message_content, client_soc
             offline_message = f'Client {target_client_id} is not online.'
             client_socket.send(offline_message.encode('utf-8'))
 
+# Function to send the list of online clients to all clients
 def send_client_list_to_all_clients():
     global clients
-
-    # Create a single message with the list of online clients
     online_clients_message = f'Online clients: {", ".join(list(clients.keys()))}'
-
-    # Send the message to all clients
     for client_id, client_socket in clients.items():
         client_socket.send(online_clients_message.encode('utf-8'))
 
+# Function to reset a client's last alive timestamp
 def reset_client_life(client_id):
     global clients_last_alive
     with clients_lock:
         clients_last_alive[client_id] = time.time()
-        
-        # Create a list of online clients
         online_clients = list(clients.keys())
-        
-        # Create a single message with the list of online clients
         online_clients_message = f'Online clients: {", ".join(online_clients)}'
-        
-        # Print the online clients message
         print(f"Alive: {client_id} - {online_clients_message}")
 
-
+# Function to remove a client from the server
 def remove_client(client_id, client_socket):
     global clients, clients_last_alive
     with clients_lock:
@@ -95,10 +93,10 @@ def remove_client(client_id, client_socket):
             del clients[client_id]
             del clients_last_alive[client_id]
             client_socket.close()  # Close the client socket
-            print(
-                f'Client {client_id} disconnected. Online clients: {list(clients.keys())}')
+            print(f'Client {client_id} disconnected. Online clients: {list(clients.keys())}')
             send_client_list_to_all_clients()
 
+# Function to periodically check if clients are still alive
 def check_client_alive():
     global clients_last_alive
     while True:
@@ -115,6 +113,7 @@ def check_client_alive():
             remove_client(client_id, client_socket)
         time.sleep(1)  # Check client activity every 1 second
 
+# Main function to set up the server and handle client connections
 def main():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind(('localhost', 12345))
@@ -122,6 +121,7 @@ def main():
 
     print('Server listening on port 12345...')
 
+    # Start a thread to periodically check client activity
     alive_check_thread = threading.Thread(target=check_client_alive)
     alive_check_thread.start()
 
@@ -131,8 +131,7 @@ def main():
         with clients_lock:
             clients[client_id] = client_socket
             clients_last_alive[client_id] = time.time()
-            print(
-                f'Client {client_id} connected. Online clients: {list(clients.keys())}')
+            print(f'Client {client_id} connected. Online clients: {list(clients.keys())}')
             send_client_list_to_all_clients()
             
         client_handler = threading.Thread(
